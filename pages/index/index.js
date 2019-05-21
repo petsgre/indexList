40058d990fb0c4906993bde0083c3dfcda45e7ce
张啸 <petsgre@gmail.com>
2019-05-22 00:02:44 +0800

    更新授权方式

--- pages/index/index.js
+++ pages/index/index.js
@@ -12,15 +12,31 @@ Page({
       url: '../logs/logs'
     })
   },
-  onLoad: function () {
+  toSearchPage: function() {
+    wx.navigateTo({
+      url: '../search/search'
+    })
+  },
+  onGotUserInfo(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+  onLoad: function() {
     console.log('onLoad')
     var that = this
     //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
+    wx.getUserInfo({
+      success: function(res) {
+        that.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
     })
+
   }
 })
